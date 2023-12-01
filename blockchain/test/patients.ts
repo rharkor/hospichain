e6e2e30612e64541e2446df70e21012579bd0851
rharkor <louis@huort.com>
2023-12-01 15:42:42 +0000

    fix: test

--- blockchain/test/patients.ts
+++ blockchain/test/patients.ts
@@ -1,36 +1,35 @@
-import {
-    time,
-    loadFixture,
-  } from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
-  import { expect } from "chai";
-  import hre from "hardhat";
-  import { getAddress, parseGwei } from "viem";
-  
-  describe("Patients", function () {
-    async function deploy() {
-      // Contracts are deployed using the first signer/account by default
-      const [owner, otherAccount] = await hre.viem.getWalletClients();
-  
-      const roleManager = await hre.viem.deployContract("RoleManager");
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox-viem/network-helpers"
+import hre from "hardhat"
 
-      const patients = await hre.viem.deployContract("Patients");
-  
-      const publicClient = await hre.viem.getPublicClient();
-  
-      return {
-        patients,
-        owner,
-        otherAccount,
-        publicClient,
-      };
+describe("Patients", function () {
+  async function deploy() {
+    // Contracts are deployed using the first signer/account by default
+    const [owner, manager1, praticien1, praticien2, account5] = await hre.viem.getWalletClients()
+
+    const roleManager = await hre.viem.deployContract("RoleManager")
+
+    await roleManager.write.addManager([manager1.account.address])
+    await roleManager.write.addPraticien([praticien1.account.address])
+    await roleManager.write.addPraticien([praticien2.account.address])
+
+    const patients = await hre.viem.deployContract("Patients", [roleManager.address])
+
+    const publicClient = await hre.viem.getPublicClient()
+
+    return {
+      patients,
+      owner,
+      manager1,
+      praticien1,
+      praticien2,
+      account5,
+      publicClient,
     }
-  
-    describe("Deployment", function () {
-      it("Should deploy correctly", async function () {
-        const { patients } = await loadFixture(deploy);
-  
-      });
-    });
+  }
 
-  });
-  
+  describe("Deployment", function () {
+    it("Should deploy correctly", async function () {
+      await loadFixture(deploy)
+    })
+  })
+})
