c16f09f852a9e2c08b22ed7621bcc341412c4ced
Juliacnc <julia.canac@gmail.com>
2023-12-07 16:08:33 +0100

    update operations

--- blockchain/contracts/operations.sol
+++ blockchain/contracts/operations.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./role-manager.sol";
+import "./praticiens.sol";
 
 contract Operations is AccessControl {
   RoleManager roleManager;
+  Praticiens praticiens;
 
   struct Operation {
     string operation_type;
-    string prescriber;
+    uint prescriber;
     string reason;
     uint result;
     string complications;
@@ -20,9 +22,10 @@ contract Operations is AccessControl {
   mapping(uint => Operation) public operations;
   uint public OperationCount;
 
-  constructor(address roleManagerAddress) {
+  constructor(address roleManagerAddress, address praticiensAddress) {
     _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     roleManager = RoleManager(roleManagerAddress);
+    praticiens = Praticiens(praticiensAddress);
   }
 
   modifier onlyPraticien() {
@@ -40,17 +43,40 @@ contract Operations is AccessControl {
     _;
   }
 
+  function compareStrings(string memory a, string memory b) public pure returns (bool) {
+    return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+  }
+
   function addOperation(Operation memory newOperation) public onlyPraticien {
+
+    if (newOperation.prescriber != 0) {
+      require(
+        compareStrings(praticiens.getPraticien(newOperation.prescriber).email, ""),
+        "Prescriber is not a praticien"
+      );
+    }
     operations[OperationCount] = newOperation;
     OperationCount++;
   }
 
-  function updateOperation(uint OperationId, Operation memory updatedOperation) public onlyPraticien {
-    require(OperationId < OperationCount, "Invalid Operation ID");
-    operations[OperationId] = updatedOperation;
+  function updateOperation(uint operationId, Operation memory updatedOperation) public onlyPraticien {
+
+    if (updatedOperation.prescriber != 0) {
+      require(
+        compareStrings(praticiens.getPraticien(updatedOperation.prescriber).email, ""),
+        "Prescriber is not a praticien"
+      );
+    }
+    require(operationId < OperationCount, "Invalid Operation ID");
+    operations[operationId] = updatedOperation;
   }
 
   function setRoleManager(address roleManagerAddress) public onlyAdmin {
     roleManager = RoleManager(roleManagerAddress);
+  }
+
+  function getOperation(uint operationId) public view returns (Operation memory) {
+    require(operationId < OperationCount, "Invalid Praticien ID");
+    return operations[operationId];
   }
 }
